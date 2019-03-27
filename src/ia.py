c43d8bbcb7bc4693bfd194dc9d44d6387e69775b
Arthur Correnson <arthur.correnson@gmail.com>
2019-03-27 20:41:13 +0100

    Minmax ab and Thread

--- src/ia.py
+++ src/ia.py
@@ -5,6 +5,10 @@
 """
 
 from main import *
+import time
+from multiprocessing import Process
+from multiprocessing import Pool
+
 
 ### Utilitaires
 
@@ -20,7 +24,7 @@ def copy(grille):
 
 ### MINMAX
 
-MDEP = 7
+MDEP = 6
 IA = 0
 EN = 1
 
@@ -117,41 +121,153 @@ def Max(grille, depth=0):
         return max_val
 
 
-### Affichage
+def minmax_ab(grille, joueur, A, B, depth=0):
+    if fin_partie(grille) or depth >= MDEP:
+        return value(grille, depth, joueur)
+    else:
+        Beta = B
+        Alpha = A
+        cpc, cpd = liste_coups_possibles(grille, IA)
+        if joueur == IA:
+            for c in cpc:
+                cp = copy(grille)
+                x1, y1, x2, y2 = c
+                deplacement_capture(x1, y1, x2, y2, IA, cp)
+                Alpha = max(Alpha, minmax_ab(cp, EN, Alpha, Beta, depth+1))
+                if Alpha >= Beta:
+                    return Beta
+            for c in cpd:
+                cp = copy(grille)
+                x1, y1, x2, y2 = c
+                deplacement_capture(x1, y1, x2, y2, IA, cp)
+                Alpha = max(Alpha, minmax_ab(cp, EN, Alpha, Beta, depth+1))
+                if Alpha >= Beta:
+                    return Beta
+            return Alpha
+        else:
+            for c in cpc:
+                cp = copy(grille)
+                x1, y1, x2, y2 = c
+                deplacement_simple(x1, y1, x2, y2, IA, cp)
+                Beta = min(Alpha, minmax_ab(cp, EN, Alpha, Beta, depth+1))
+                if Alpha >= Beta:
+                    return Alpha
+            for c in cpd:
+                cp = copy(grille)
+                x1, y1, x2, y2 = c
+                deplacement_simple(x1, y1, x2, y2, IA, cp)
+                Beta = min(Alpha, minmax_ab(cp, EN, Alpha, Beta, depth+1))
+                if Alpha >= Beta:
+                    return Alpha
+            return Beta
+
+
+def run():
+    grille = grille_debut_partie()
+    cpc, cps = liste_coups_possibles(grille, IA)
+    meilleur_coup = None
+    meilleur_score = 0
+    debut = time.time()
+
+    for c in cpc:
+        cp = copy(grille)
+        x1, y1, x2, y2 = c
+        deplacement_capture(x1, y1, x2, y2, IA, cp)
+
+        score =  Min(cp)
+        if score > meilleur_score:
+            meilleur_score = score
+            meilleur_coup = c
+
+    for c in cps:
+        cp = copy(grille)
+        x1, y1, x2, y2 = c
+        deplacement_simple(x1, y1, x2, y2, IA, cp)
+        score = Min(cp)
+        if score > meilleur_score:
+            meilleur_score = score
+            meilleur_coup = c
+    
+    print(meilleur_coup)
+
+    print('Time :')
+    print(time.time() - debut)
+
+
+def job(t):
+    coup, grille = t
+    cp = copy(grille)
+    x1, y1, x2, y2 = coup
+    deplacement_capture(x1, y1, x2, y2, IA, cp)
+    score = Min(cp)
+    return score
+
+
+def test():
+    grille = grille_debut_partie()
+    cpc, cps = liste_coups_possibles(grille, IA)
+    meilleur_coup = None
+    meilleur_score = 0
+    debut = time.time()
+    
+    p = Pool(processes=50)
+    
+    print(p.map(job, zip(cpc, [grille for i in range(len(cpc))])))
+    print(p.map(job, zip(cpc, [grille for i in range(len(cps))])))
+    
+
 
 grille = [
-    ['O', ' ', 'X', 'O'],
+    ['O', 'X', ' ', 'O'],
     ['X', 'X', 'X', 'X'],
     [' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' '],
 ]
 
-grille = grille_debut_partie()
 
-cpc, cps = liste_coups_possibles(grille, IA)
+def tour_ia_boost(grille, joueur):
+    
+    cpc, cps = liste_coups_possibles(grille, IA)
+    meilleur_coup = None
+    meilleur_score = 0
 
-afficher_grille(grille)
+    for c in cpc:
+        cp = copy(grille)
+        x1, y1, x2, y2 = c
+        deplacement_capture(x1, y1, x2, y2, IA, cp)
+        score =  minmax_ab(cp, EN, -1234, 1234)
+        if score > meilleur_score:
+            meilleur_score = score
+            meilleur_coup = c
 
-meilleur_coup = None
-meilleur_score = 0
+    for c in cps:
+        cp = copy(grille)
+        x1, y1, x2, y2 = c
+        deplacement_simple(x1, y1, x2, y2, IA, cp)
+        score =  minmax_ab(cp, EN, -1234, 1234)
+        if score > meilleur_score:
+            meilleur_score = score
+            meilleur_coup = c
 
-for c in cpc:
-    cp = copy(grille)
-    x1, y1, x2, y2 = c
-    deplacement_capture(x1, y1, x2, y2, IA, cp)
-    score = Min(cp)
-    if score > meilleur_score:
-        meilleur_score = score
-        meilleur_coup = c
+    if meilleur_coup is None:
+        if cpc != []:
+            meilleur_coup = cpc[0]
+        else:
+            meilleur_coup = cps[0]
 
-for c in cps:
-    cp = copy(grille)
-    x1, y1, x2, y2 = c
-    deplacement_simple(x1, y1, x2, y2, IA, cp)
-    score = Min(cp)
-    if score > meilleur_score:
-        meilleur_score = score
-        meilleur_coup = c
+    print(meilleur_coup, meilleur_score)
+    
+    x1, y1, x2, y2 = meilleur_coup
+    if deplacement_capture(x1, y1, x2, y2, IA, grille):
+        print('blop')
+    else:
+        deplacement_simple(x1, y1, x2, y2, IA, grille)
 
-print("Le meilleur coup est : ")
-print(meilleur_coup)
+mode_jeu = 1
+
+while not fin_partie(grille):
+    if mode_jeu == 1:
+        tour_ia_boost(grille, IA)
+    else:
+        tour_de_jeu(grille, IA)
+    tour_de_jeu(grille, EN)
